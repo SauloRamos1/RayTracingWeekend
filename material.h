cd2fb60ea167d9294583ec2e60dc1a67275ca82c
Saulo Ramos <srdcp@hotmail.com>
2026-09-14 14:31:02 -0600

    Fix Schlick reflectance approximation in dielectric
    Reflectance used r0 * (1 - r0) * (1 - cos)^5 instead of
    r0 + (1 - r0) * (1 - cos)^5, so glass showed almost no Fresnel
    reflection.

--- material.h
+++ material.h
@@ -79,7 +79,7 @@ private:
     static double reflectance (double cosine, double refraction_index) {
         auto r0 = (1 - refraction_index) / (1 + refraction_index);
         r0 = r0*r0;
-        return r0 * (1-r0)*std::pow((1 - cosine), 5);
+        return r0 + (1-r0)*std::pow((1 - cosine), 5);
     }
 };
 
